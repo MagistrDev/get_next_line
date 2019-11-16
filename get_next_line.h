867befc3c02dd906185967f6daee9a28eb3b1950
Elena Celsa <ecelsa@vo-h4.21-school.ru>
2019-11-17 02:55:41 +0300

    dfsdfsdfs

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: ecelsa <ecelsa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 22:32:09 by ecelsa            #+#    #+#             */
-/*   Updated: 2019/11/17 00:39:41 by ecelsa           ###   ########.fr       */
+/*   Updated: 2019/11/17 02:53:55 by ecelsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1600
+# define BUFF_SIZE 10000
 # define ARR_SIZE 12000
 # include "libft/libft.h"
 # include <fcntl.h>
